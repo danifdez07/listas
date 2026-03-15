6d08b8b0979c85d97d64c8f0d56d522d7f637407
Daniel Fernandez-Espartero Ortiz <daniel.fernandeze@edu.uah.es>
2026-03-15 04:21:37 +0100

    añadir abstracciones

--- src/ListasEstructuradeDatos/ListaSimplementeEnlazada/Lista.java
+++ src/ListasEstructuradeDatos/ListaSimplementeEnlazada/Lista.java
@@ -79,4 +79,118 @@ public class Lista<T> implements ListaTAD<T> {
 
         return actual;
     }
+    public void invertir(){
+        if(cabeza == null){
+            return;
+        }
+        Nodo<T> anterior = null;
+        Nodo<T> actual = cabeza;
+        Nodo<T> siguiente = null;
+        while(actual!=null){
+            actual = cabeza.getSiguiente();
+            actual.setSiguiente(anterior);
+            anterior = actual;
+            actual = siguiente;
+        }
+        this.cabeza = anterior;
+    }
+    public T obtenerCentral(){
+        if(this.cabeza == null){
+            return null;
+        }
+        Nodo<T> medio=this.cabeza;
+        Nodo<T> fin=this.cabeza;
+        while (fin != null && fin.getSiguiente()!=null){
+            medio = medio.getSiguiente();
+            fin = fin.getSiguiente().getSiguiente();
+        }
+        return medio.getDato();
+
+    }
+    public void EliminarRepetidos(){
+        if(this.cabeza == null) {
+            return;
+        }
+        Nodo<T> actual = this.cabeza;
+        while(actual != null){
+            Nodo<T> detector = actual;
+            while(detector.getSiguiente()!=null) {
+                if (detector.getSiguiente().getDato().equals(actual.getDato())) {
+                    detector.setSiguiente(detector.getSiguiente().getSiguiente());
+                    tamano--;
+                } else {
+                    detector = detector.getSiguiente();
+                }
+            }
+            actual = actual.getSiguiente();
+        }
+
+    }
+    public boolean detectorCiclo(){//misma idea que en el de buscar el centro
+        if(this.cabeza == null){
+            return false;
+        }
+        Nodo<T> medio = this.cabeza;
+        Nodo<T> fin = this.cabeza;
+        while(fin.getSiguiente()!=null){
+            medio=medio.getSiguiente();
+            fin=fin.getSiguiente().getSiguiente();
+            if(medio==fin){
+                return true;
+            }
+
+
+        }
+        return false;
+
+    }
+    public void EliminarPorElFinal(int n){//hay q contar desde atras para eliminar
+        if(this.cabeza == null || n<0){
+            return;
+        }
+        Nodo<T> localizar = this.cabeza;
+        Nodo<T> eliminador = this.cabeza;
+        for(int i=0;i<n;i++  ){
+            if(localizar ==null) return;
+            localizar = localizar.getSiguiente();
+        }
+        if(localizar == null){
+            this.cabeza = this.cabeza.getSiguiente();
+            tamano --;
+            return;
+        }
+        while(localizar.getSiguiente()!=null){
+            localizar =localizar.getSiguiente();
+            eliminador=eliminador.getSiguiente();
+        }
+        eliminador.setSiguiente(eliminador.getSiguiente().getSiguiente());
+        tamano--;
+
+    }
+    public void intercalar(ListaTAD<T> otraLista) {//intercala listas
+        if (!(otraLista instanceof Lista<?>)) return;
+
+
+        Lista<T> otraListaReal = (Lista<T>) otraLista;
+
+        Nodo<T> miActual = this.cabeza;
+        Nodo<T> otroActual = otraListaReal.cabeza;
+        Nodo<T> miSiguiente;
+        Nodo<T> otroSiguiente;
+
+        while (miActual != null && otroActual != null) {
+            miSiguiente = miActual.getSiguiente();
+            otroSiguiente = otroActual.getSiguiente();
+
+            miActual.setSiguiente(otroActual);
+            otroActual.setSiguiente(miSiguiente);
+
+            miActual = miSiguiente;
+            otroActual = otroSiguiente;
+        }
+
+        this.tamano += otraListaReal.tamano;
+        otraListaReal.cabeza = null;
+        otraListaReal.tamano = 0;
+    }
 }
